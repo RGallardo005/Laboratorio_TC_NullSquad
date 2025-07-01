f92a36a8b2c9ffb32a4cbf6387db00fc6f225cf9
CamilaCC <cacoreaco@est.utn.ac.cr>
2025-07-01 16:01:59 -0600

    Modificacion clase TrabajadorSocial
    Correxión de errores por sintacxis

--- src/TrabajadoresSociales/TrabajadorSocial.java
+++ src/TrabajadoresSociales/TrabajadorSocial.java
@@ -12,10 +12,11 @@ public class TrabajadorSocial extends Persona {
     
     private String canton;
     
-    public TrabajadorSocial(String cedula, String nombre, String correo, String telefono, String canton){
-        super(cedula,nombre, correo, telefono);
+    public TrabajadorSocial(String nombre, int id, String email, String telefono, String canton){
+        super(nombre, id, email, telefono);
         this.canton = canton; 
     }
+
     
     public String getCanton(){
         return canton; 
@@ -26,6 +27,7 @@ public class TrabajadorSocial extends Persona {
     }
 
     
+    @Override
     public String toString() {
         return "TrabajadorSocial{" + "canton=" + canton + '}';
     }
